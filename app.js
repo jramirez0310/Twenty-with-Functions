5229f7b577d4b4f52b982c6d2f025c6e2f7ec6f3
Ramirez <jramirez1@FVFZMXVMJ1WK.tld>
2022-03-25 10:58:45 -0500

    Added User Num (NOT COMPLETE)

--- app.js
+++ app.js
@@ -8,3 +8,35 @@ function greeting(a){
 }
 
 greeting(askName())
+
+function userNum(){
+    h_num = Math.floor(Math.random() * 10) + 1
+
+    h_total = 0 + h_num
+
+    alert(`You drew ${h_num}. Your total is ${h_total}`)
+
+    do{
+        h_choice = prompt("Would you like to draw again? (y for yes, n for no)")
+
+        if (h_choice == "y"){
+
+            h_num = Math.floor(Math.random() * 10) + 1
+
+            h_total = h_total + h_num
+            
+        } else if (h_choice == "n"){
+
+            break
+
+        } else {
+
+            alert("I said put in 'y' or 'n'. Next time, DO WHAT YOU ARE TOLD!")
+
+        }
+    }while (h_total < 20)
+
+    return h_total
+}
+
+alert(userNum())
